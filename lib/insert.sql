102543546fc7c65c91c36b30519f0bb8c739aff2
vchemu <valerine.chemutai@student.moringaschool.com>
2023-10-29 13:19:45 +0300

    bears_lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Brown", "gentle", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 12, "M", "Black", "Aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "F", "Light-brown", "playful", 2);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Seargent Brown", 13, "M", "Brown", "social", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 21, "F", "White", "joyful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 23, "M", "blue-gray", "angry", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 10, "F", "Cinammon", "altruistic", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 8, "F", "Blond", "affectionate", 1);
